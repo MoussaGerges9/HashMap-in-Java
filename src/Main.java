6f319229981bdf3e288c12955b1c1a028580f31f
Moussa Gerges <moussagerges9@gmail.com>
2021-02-28 01:05:54 +0200

    Add and Replace functions

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
+import java.util.HashMap;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		HashMap<Integer, String> map = new HashMap<Integer, String>();
+		map.put(1220, "Liam");
+		map.put(1221, "Noah");
+		map.put(1222, "Oliver");
+		map.put(1223, "Noah");
+		map.put(1224, "Matthew");
+		map.put(1225, "Joseph");
 
+		System.out.println("Initialized map: " + map);
+		System.out.println("Current map size: " + map.size());
+
+		// Replace values
+		map.put(1222, "Luca");
+		map.replace(1224, "Jose");
+		System.out.println("Map after modifying: " + map);
 	}
 
 }
